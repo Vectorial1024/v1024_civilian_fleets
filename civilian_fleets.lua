545d726c18178ac2a4bbf5fe8ee03edfb216772e
Vectorial1024 <vincentwwy168@gmail.com>
2020-04-10 19:03:13 +0800

    v1.12: Update to match game version 3.00

--- civilian_fleets.lua
+++ civilian_fleets.lua
@@ -2,6 +2,10 @@
 local ffi = require("ffi")
 local C = ffi.C
 
+--[[
+As a quick deployment note, the relevant assignment Lua script is located at /ui/addons/ego_interactmenu/*.lua
+]]--
+
 local function initialize()
     DebugError("V1024 Civilian Fleets, UI component starting.")
 
@@ -10,6 +14,7 @@ end
 
 function V1024CF_CivilianAssignment(menu)
     -- We need to verify whether the assignment is valid, before allowing the game to add in interaction contents.
+    --DebugError("L1")
     if menu.numassignableships > 0 then
         --[[
         Mining ships can be assigned to other mining ships as Mining/Trading ship
@@ -17,10 +22,30 @@ function V1024CF_CivilianAssignment(menu)
         (Currently we don't have hybrid-storage ships, but just be prepared.)
         ]]--
 
-        if (not C.IsComponentClass(menu.componentSlot.component, "station")) then
+        --DebugError("L2")
+        local isstation = C.IsComponentClass(menu.componentSlot.component, "station")
+        local isship = C.IsComponentClass(menu.componentSlot.component, "ship")
+
+        if (isship) then
             -- The mod only applies when assigning to ships.
+            --DebugError("L3")
             
             local convertedComponent = ConvertStringTo64Bit(tostring(menu.componentSlot.component))
+
+            --v3.0: every subordinate must belong to some battle groups
+            local subordinates = GetSubordinates(convertedComponent)
+            local groups = {}
+            for _, subordinate in ipairs(subordinates) do
+                local group = GetComponentData(subordinate, "subordinategroup")
+                if group and group > 0 then
+                    if groups[group] then
+                        table.insert(groups[group].subordinates, subordinate)
+                    else
+                        groups[group] = { assignment = ffi.string(C.GetSubordinateGroupAssignment(menu.componentSlot.component, group)), subordinates = { subordinate } }
+                    end
+                end
+            end
+
             local commanderCanMine = (GetComponentData(convertedComponent, "primarypurpose") == "mine")
             -- Intuitive.
             local selectedMiningShips = (menu.numassignableminingships > 0)
@@ -31,7 +56,7 @@ function V1024CF_CivilianAssignment(menu)
 
             local enableMiningFleet = (commanderCanMine and selectedMiningShips)
             local enableTradingFleet = (((not commanderCanMine) and selectedTradingShips) or enableMiningFleet)
-
+            --DebugError("Dumping vars: ." .. tostring(convertedComponent) .. ", " .. tostring(commanderCanMine) .. ", " .. tostring(selectedMiningShips) .. ", " .. tostring(enableMiningFleet) .. ", " .. tostring(enableTradingFleet) .. ";")
             -- Feature not unlocked yet.
             --enableMiningFleet = false
 
@@ -42,12 +67,15 @@ function V1024CF_CivilianAssignment(menu)
                 If Commander cannot mine, and we have selected some non-mining ships, then enable "Trading Fleet".
             ]]--
 
-            --menu.insertInteractionContent("selected_assignments", { type = actiontype, text =  "[" .. ReadText(1001, 7814) .. "] " .. ReadText(20208, 40303), text2 = Helper.convertColorToText(menu.holomapcolor.playercolor) .. ((menu.numassignableships == 1) and ReadText(1001, 7851) or string.format(ReadText(1001, 7801), menu.numassignableships)), script = function () menu.buttonAssignCommander("defence") end })
-            -- Form/join trading fleet
-            menu.insertInteractionContent("selected_assignments", { type = actiontype, text = ReadText(221024, 1000), text2 = Helper.convertColorToText(menu.holomapcolor.playercolor) .. ((menu.numassignableships == 1) and ReadText(1001, 7851) or string.format(ReadText(1001, 7801), menu.numassignableships)), script = function () menu.buttonAssignCommander("trade") end, active = enableTradingFleet })
-            -- Form/join mining fleet
-            menu.insertInteractionContent("selected_assignments", { type = actiontype, text = ReadText(221024, 2000), text2 = Helper.convertColorToText(menu.holomapcolor.playercolor) .. ((menu.numassignableships == 1) and ReadText(1001, 7851) or string.format(ReadText(1001, 7801), menu.numassignableships)), script = function () menu.buttonAssignCommander("mining") end, active = enableMiningFleet })
-            --menu.insertInteractionContent("selected_assignments", { type = actiontype, text = "Form/join mining fleet", text2 = tostring(commanderCanMine) .. " " .. tostring(selectedMiningShips), script = function () menu.buttonAssignCommander("mining") end, active = enableMiningFleet })
+            --We have a new fleet system in v3.0, so we rewrite this part
+            --Luckily, this results in me having to do less work.
+            --menu.insertAssignSubActions("selected_assignments_attack", "attack", menu.buttonAssignCommander, groups, isstation)
+            if (enableMiningFleet) then
+                menu.insertAssignSubActions("selected_assignments_mining", "mining", menu.buttonAssignCommander, groups, isstation)
+            end
+            if (enableTradingFleet) then
+                menu.insertAssignSubActions("selected_assignments_trade", "trade", menu.buttonAssignCommander, groups, isstation)
+            end
         end
 	end
 end
